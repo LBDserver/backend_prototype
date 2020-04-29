292f3a2f7a17050f119dc5c31a2df401d5d2fddd
Jeroen Werbrouck <jeroen.werbrouck@ugent.be>
2020-04-29 14:57:04 +0200

    acl modes and scenarios

--- src/authApi/checkPermissions.js
+++ src/authApi/checkPermissions.js
@@ -19,10 +19,30 @@ checkPermissions = (req) => {
 getAcl = (req) => {
     return new Promise(async (resolve, reject) => {
         try {
-            let meta
-            resolve()
+            let meta, acl
+            // check which acl file is used for the resource and action
+                // create project: none
+                // update project: project WRITE
+                // delete project: project CONTROL
+                // read project: project READ
+                
+                // create named graph: project WRITE
+                // read named graph: graph READ
+                // update named graph: graph WRITE
+                // delete named graph: graph CONTROL
 
+                // create document: project WRITE
+                // read document: document READ
+                // delete document: document CONTROL
+                // change acl: document CONTROL
 
+                // ? this poses an issue, as a acl file can be used by multiple resources.
+                // => ! what if an acl is used by multiple graphs => create duplicate, change duplicate and refer to duplicate. Else: change acl graph
+                // if the project acl has multiple owners, all of them must agree on changing it.
+                // read acl file: CONTROL
+                // change acl file: CONTROL
+
+            resolve(acl)
         } catch (error) {
             console.log('error', error)
             reject(error)
